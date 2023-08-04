3573da8003401cbefe0898de0013c717c35cfc88
qmars <qmars@outlook>
2023-08-03 21:36:55 -0400

    add all images scss files and navbar

---
+++ index.php
@@ -0,0 +1,8 @@
+<html lang="en">
+    <?php include 'components/headtag.html'; ?>
+<body>
+    <!------------------------------[navbar]------------------------------>
+    <?php include 'components/navbar.html'; ?>
+    <!------------------------------[navbar]------------------------------>
+</body>
+</html>
