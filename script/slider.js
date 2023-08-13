6fe3b0c09214a339cb31f5ee7451ca09dc0aa46b
qmars <qmars@outlook>
2023-08-13 16:38:12 -0400

    navbar responsibily is done just ned hamburger menu

--- script/slider.js
+++ script/slider.js
@@ -1,75 +1,75 @@
-const sliderItems = document.querySelectorAll(".header-slider-item");
-const radioInputs = document.querySelectorAll(".header-slider-radio-item");
+// const sliderItems = document.querySelectorAll(".header-slider-item");
+// const radioInputs = document.querySelectorAll(".header-slider-radio-item");
 
-let currentIndex = 0;
-let intervalId;
-let isDragging = false;
-let startX = 0;
+// let currentIndex = 0;
+// let intervalId;
+// let isDragging = false;
+// let startX = 0;
 
-function showSlide(index) {
-    sliderItems.forEach(item => {
-        item.classList.remove("active");
-    });
+// function showSlide(index) {
+//     sliderItems.forEach(item => {
+//         item.classList.remove("active");
+//     });
 
-    sliderItems[index].classList.add("active");
-    radioInputs[index].checked = true;
-}
+//     sliderItems[index].classList.add("active");
+//     radioInputs[index].checked = true;
+// }
 
-function rotateSlider() {
-    currentIndex = (currentIndex + 1) % sliderItems.length;
-    showSlide(currentIndex);
-}
+// function rotateSlider() {
+//     currentIndex = (currentIndex + 1) % sliderItems.length;
+//     showSlide(currentIndex);
+// }
 
-function startSlider() {
-    showSlide(currentIndex);
-    intervalId = setInterval(rotateSlider, 2000);
-    radioInputs[currentIndex].checked = true;
-}
+// function startSlider() {
+//     showSlide(currentIndex);
+//     intervalId = setInterval(rotateSlider, 5000000000000);
+//     radioInputs[currentIndex].checked = true;
+// }
 
-function stopSlider() {
-    clearInterval(intervalId);
-}
+// function stopSlider() {
+//     clearInterval(intervalId);
+// }
 
-function activateSlider(index) {
-    stopSlider();
-    currentIndex = index;
-    showSlide(index);
-    startSlider();
-}
+// function activateSlider(index) {
+//     stopSlider();
+//     currentIndex = index;
+//     showSlide(index);
+//     startSlider();
+// }
 
-function handleMouseDown(event) {
-    isDragging = true;
-    startX = event.clientX;
-}
+// function handleMouseDown(event) {
+//     isDragging = true;
+//     startX = event.clientX;
+// }
 
-function handleMouseMove(event) {
-    if (!isDragging) return;
-    const deltaX = event.clientX - startX;
-    if (deltaX > 50) {
-        activateSlider((currentIndex - 1 + sliderItems.length) % sliderItems.length);
-        isDragging = false;
-    } else if (deltaX < -50) {
-        activateSlider((currentIndex + 1) % sliderItems.length);
-        isDragging = false;
-    }
-}
+// function handleMouseMove(event) {
+//     if (!isDragging) return;
+//     const deltaX = event.clientX - startX;
+//     if (deltaX > 50) {
+//         activateSlider((currentIndex - 1 + sliderItems.length) % sliderItems.length);
+//         isDragging = false;
+//     } else if (deltaX < -50) {
+//         activateSlider((currentIndex + 1) % sliderItems.length);
+//         isDragging = false;
+//     }
+// }
 
-function handleMouseUp() {
-    isDragging = false;
-}
+// function handleMouseUp() {
+//     isDragging = false;
+// }
 
-sliderItems.forEach((item, index) => {
-    item.addEventListener("mousedown", handleMouseDown);
-    item.addEventListener("mousemove", handleMouseMove);
-    item.addEventListener("mouseup", handleMouseUp);
-});
+// sliderItems.forEach((item, index) => {
+//     item.addEventListener("mousedown", handleMouseDown);
+//     item.addEventListener("mousemove", handleMouseMove);
+//     item.addEventListener("mouseup", handleMouseUp);
+// });
 
-radioInputs.forEach((input, index) => {
-    input.addEventListener("change", function () {
-        if (this.checked) {
-            activateSlider(index);
-        }
-    });
-});
+// radioInputs.forEach((input, index) => {
+//     input.addEventListener("change", function () {
+//         if (this.checked) {
+//             activateSlider(index);
+//         }
+//     });
+// });
 
-startSlider();
+// startSlider();
