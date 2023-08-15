9a6b86c79ea287a1dd761211c6f170f62c6a4577
qmars <qmars@outlook>
2023-08-15 08:58:36 -0400

    warecard json and js is ready and its responsive

--- script/custom.js
+++ script/custom.js
@@ -1,4 +1,67 @@
-$('.myqmars').slick({
-    dots:true,
-    centerMode: true,
+$(".myqmars").slick({
+    dots: true,
+    centerMode: true
 });
+
+// ----------------[warecard builder]------------------
+$.getJSON("../data/waresInfo.json", function (data) {
+    $.each(data, function (index, item) {
+        const article = $("<article>", { class: "col-md-3 p-2 col-6" }).append(
+            $("<div>", { class: "card warecard" }).append(
+                $("<div>", { class: "warecard-banner" }).append(
+                    $("<img>", {
+                        class: "card-img-top warecard-banner-img",
+                        src: item.imgPath,
+                        alt: item.imgAlt
+                    })
+                ),
+                $("<div>", { class: "position-absolute warecard-img-panel p-2 m-2 bg-white shadow-sm" }).append(
+                    $("<ul>").append(
+                        $("<li>", { class: "p-2 text-size-x" }).append(
+                            $("<a>", { href: "#" }).append($("<i>", { class: "fa-solid fa-shuffle" }))
+                        ),
+                        $("<li>", { class: "p-2 text-size-x" }).append(
+                            $("<a>", { href: "#" }).append($("<i>", { class: "fa-solid fa-magnifying-glass" }))
+                        ),
+                        $("<li>", { class: "p-2 text-size-x" }).append(
+                            $("<a>", { href: "#" }).append($("<i>", { class: "fa-regular fa-heart" }))
+                        )
+                    )
+                ),
+                $("<div>", { class: "card-body py-1 px-3" }).append(
+                    $("<a>", { class: "card-title pt-5", href: "#" }).text(item.title),
+                    $("<a>", { class: "text-muted d-block pt-1", href: "#" }).text(item.categories),
+
+                    $("<ul>", { class: "card-text py-1" }).append(
+                        // Loop through sizes and create list items
+                        (function () {
+                            const sizeItems = [];
+                            $.each(item.sizes, function (index, size) {
+                                sizeItems.push(
+                                    $("<li>", { class: "d-inline text-size-m" }).append($("<a>", { class: "me-3", href: "#" }).text(size))
+                                );
+                            });
+                            return sizeItems;
+                        })()
+                    ),
+
+                    $("<div>").append(
+                        $("<strong>", { class: "d-block" }).text("$199.00"),
+                        $("<a>", { class: "lhu-link my-1", href: "#" }).text("SELECT OPTION")
+                    )
+                )
+            )
+        );
+        $(".warecards").append(article);
+    });
+
+   
+    $(".warecards").on("mouseenter", ".warecard-banner", function () {
+      const hoverPath = $(this).find('.warecard-banner-img').attr('data-hover');
+      $(this).find('.warecard-banner-img').attr('src', hoverPath);
+    }).on("mouseleave", ".warecard-banner", function () {
+      const imgPath = $(this).find('.warecard-banner-img').attr('data-img');
+      $(this).find('.warecard-banner-img').attr('src', imgPath);
+    });
+});
+// ------------[warecard builder changer]--------------
